a27585f7a7b9de3cc079853b06680ef02164d155
abymoen <a.bymoen@hotmail.com>
2021-12-29 22:08:17 -0700

    Getting ready for students

---
+++ src/components/DisneyCharacter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <!-- TODO-3 Display the details of the character here -->
+  </div>
+</template>
+
+<script>
+export default {
+  // TODO-2 Expect an object as a prop that describes the character
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <section class="tv_characters">
+      <h1>TV Characters</h1>
+      <!-- TODO-4 Show only characters that have appeared in at least 1 TV show here -->
+    </section>
+
+    <section class="movie_characters">
+      <h1>Movie Characters</h1>
+      <!-- TODO-5 Show only characters that have appeared in at least 1 movie here -->
+    </section>
+
+    <section class="video_game_characters">
+      <h1>Video Game Characters</h1>
+      <!-- TODO-6 Show only characters that have appeared in at least 1 Video Game here -->
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+
+  // TODO-1 Grab the 'characters' from the store (there is hard-coded data in there you can go inspect)
+};
+</script>
